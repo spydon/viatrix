2f82ffac7b8c1379d771e84a58e2ce3ce5b5acb5
Bas van den Heuvel <bas.vandenheuvel@student.uva.nl>
2014-09-24 13:56:19 +0200

    Crates instead of casks

--- beercount.py
+++ beercount.py
@@ -3,44 +3,43 @@ import tkinter as tk
 
 
 beers = 0
-casks = 0
+crates = 0
 
 
 def display_beers(device, beers):
     viatrix.message(device, 'A E A 1 E Bier: <AB>%d' % (beers))
 
 
-def display_casks(device, casks):
-    viatrix.message(device, 'B E A 1 E Fusten: <AB>%d' % (casks))
+def display_crates(device, crates):
+    viatrix.message(device, 'B E A 1 E Krat: <AB>%d' % (crates))
 
 
 def count():
-    global beers, casks
+    global beers, crates
 
     device = viatrix.Device.create()
 
     beers = int(input('Enter beers already sold: '))
-    casks = int(input('Enter casks already emptied: '))
+    crates = int(input('Enter crates already emptied: '))
 
     def onKeyPress(event):
-        global beers, casks
+        global beers, crates
 
-        try:
-            char = ord(event.char)
-        except TypeError:
-            return
-
-        if char == 32:
-            beers += 1
+        if event.char in [' ', '-']:
+            if event.char == ' ':
+                beers += 1
+            elif event.char == '-':
+                beers -= 1
             text.delete('1.0', '2.0')
             text.insert('0.0', 'Beers: %d\n' % (beers))
             display_beers(device, beers)
 
-        elif char == 13:
-            casks += 1
-            text.delete('2.0', '3.0')
-            text.insert('2.0', 'Casks: %d\n' % (casks))
-            display_casks(device, casks)
+            new_crates = beers / 24
+            if new_crates != crates:
+                crates = new_crates
+                text.delete('2.0', '3.0')
+                text.insert('2.0', 'Crates: %d\n' % (crates))
+                display_crates(device, crates)
 
     root = tk.Tk()
 
@@ -49,10 +48,10 @@ def count():
     text.pack()
 
     text.insert('1.0', 'Beers: %d\n' % (beers))
-    text.insert('2.0', 'Casks: %d\n' % (casks))
+    text.insert('2.0', 'Crates: %d\n' % (crates))
 
     display_beers(device, beers)
-    display_casks(device, casks)
+    display_crates(device, crates)
 
     root.bind('<KeyPress>', onKeyPress)
     root.mainloop()
