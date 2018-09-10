5a26b608716cf643b301ff1e3c4efa97bca8fefc
Lukas Klingsbo <lukas.klingsbo@klarna.com>
2018-09-10 13:16:30 +0200

    Configured timing for updates

--- pgwcount.py
+++ pgwcount.py
@@ -14,21 +14,21 @@ def display_pgw_amount(device, page, amount):
 def count():
     device = viatrix.Device.create()
     buffer = list(string.ascii_uppercase)[2:]
-    viatrix.message(device, 'A E A 2 E  ')
+    viatrix.message(device, 'A E A 1 E  ')
     viatrix.message(device, 'B E A 2 E Recv. today:')
 
     data = [1086864, 1616272, 1999652, 2275432, 2522875, 2853205,
             3477041, 4682007, 6858673, 10048661, 13789367, 17544148,
             20730371, 23947612, 27063455, 29984401, 32888173, 35855263,
-            39095658, 42512301, 46663195, 51570386, 55430182, 57696709]
+            39095658, 42512301, 46663195, 51570386, 55430182, 56000222]
 
     try:
         refresh = int(input('Enter refresh time (default: 40): '))
     except ValueError:
-        refresh = 40
+        refresh = 39.7
 
     start_hour = datetime.now().hour
-    start_minute = datetime.now().minute
+    start_minute = datetime.now().minute+1
     amount = int(data[start_hour]+((data[start_hour+1]-data[start_hour])/(60/start_minute)))
     while(True):
         now = datetime.now()
@@ -52,7 +52,6 @@ def count():
 
         viatrix.schedule(device, string.ascii_uppercase)
         time.sleep(refresh)
-        print(amount)
 
     device.close()
 
